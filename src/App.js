32b48df80fe50f87069915863960a15e7fa37945
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-05 17:02:17 +0100

    integrate the navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Profile from "./pages/Profile";
 import './app.css';
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
       </BrowserRouter>
 
       {/* navbar */}
-
+      <NavBar />
       {/* side bar */}
 
       {/* outlet */}
