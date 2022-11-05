2d8763210f151f25bed42e1776fdf4160b925445
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-05 17:37:21 +0100

    integrate the sidebar component

---
+++ src/components/SideBar/SideBar.js
@@ -0,0 +1,21 @@
+import CopyRight from './../CopyRight/CopyRight';
+import Muscul from './../../assets/images/muscul.svg';
+import Swim from './../../assets/images/swim.svg';
+import Sycl from './../../assets/images/sycl.svg';
+import Yoga from './../../assets/images/yoga.svg';
+
+function SideBar() {
+    return (
+        <div className='sidebar'>
+            <div className='sidebar-icon-container'>
+                <div><img alt='yoga icon' src={Yoga} className='sidebar-icon' /></div>
+                <div><img alt='swim icon' src={Swim} className='sidebar-icon' /></div>
+                <div><img alt='sycl icon' src={Sycl} className='sidebar-icon' /></div>
+                <div><img alt='muscul icon' src={Muscul} className='sidebar-icon' /></div>
+            </div>
+            <CopyRight />
+        </div>
+    )
+}
+
+export default SideBar
