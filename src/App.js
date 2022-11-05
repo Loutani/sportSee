e2f5a8a6805991869cac76db3ccf9cdbd97b1fa1
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-05 16:42:27 +0100

    integrate the routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Profile from "./pages/Profile";
+
 function App() {
   return (
     <>
+      {/* router */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" end element={<Profile />}/>
+        </Routes>
+      </BrowserRouter>
+      
+      {/* navbar */}
 
+      {/* side bar */}
+
+      {/* outlet */}
     </>
   );
 }
