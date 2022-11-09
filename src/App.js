857873706e09e28a68a95f8d439688dff63cac1f
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-09 21:03:49 +0100

    add error route

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
+import React, { useState } from "react";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import Profile from "./pages/Profile";
 import './app.css';
 import NavBar from "./components/NavBar/NavBar";
 import SideBar from "./components/SideBar/SideBar";
+import Error from "./pages/Error";
 
+//export ShowNavBarContext context
+export const ShowNavBarContext = React.createContext();
+
+/**
+ * Component for render the application interface
+ * 
+ * @returns {React.ReactElement} 
+ */
 function App() {
+  //state to handle if the nav bar and side bar is show or hide
+  const [showNavBar, setShowNavBar] = useState(true);
+
   return (
     <>
-      {/* navbar */}
-      <NavBar />
+      <ShowNavBarContext.Provider value={{show: showNavBar, setShow: setShowNavBar}}>
+        {/* navbar */}
+        {showNavBar && <NavBar />}
 
-      {/* side bar */}
-      <SideBar />
+        {/* side bar */}
+        {showNavBar && <SideBar />}
 
-      {/* router */}
-      <BrowserRouter>
-        <Routes>
-          <Route path="/user/:id" end element={<Profile />}/>
-        </Routes>
-      </BrowserRouter>
+        {/* router */}
+        <BrowserRouter>
+          <Routes>
+            <Route path="/user/:id" end element={<Profile />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </BrowserRouter>
 
-      {/* outlet */}
-      <Outlet />
+        {/* outlet */}
+        <Outlet />
+      </ShowNavBarContext.Provider>
     </>
   );
 }
 
+//export the App component
 export default App;
