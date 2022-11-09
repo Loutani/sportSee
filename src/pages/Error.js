561171e2d922ad5e9a76f76b8120983c89ea4654
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-09 21:03:33 +0100

    add Error page

---
+++ src/pages/Error.js
@@ -0,0 +1,25 @@
+import { useContext } from "react"
+import { ShowNavBarContext } from "../App"
+
+/**
+ * Component for render the Error page
+ * 
+ * @returns {React.ReactElement}
+ */
+function Error() {
+    //context to use the show nav bar global state
+    const showNavBar = useContext(ShowNavBarContext);
+
+    //hide the nav bar and side bar by set the state to false
+    showNavBar.setShow(false);
+
+    return (
+        <div>
+            <h1>Error:</h1>
+            <p>Something went wrong</p>
+        </div>
+    )
+}
+
+//export Error component
+export default Error
