6c3450757f2b5596ea3c54b18d2f070b20cc5275
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-13 20:46:25 +0100

    integrate the env file

---
+++ src/config/env.js
@@ -0,0 +1,7 @@
+/**
+ * @param {object} Env - this is the envirement setup 
+ * @param {('mock'|'api')} Env.envirement this is the envirement type
+ */
+export const Env = {
+    envirement: 'mock'
+}
